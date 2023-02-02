cd7dc9c41824803a0a0b2b50510a95b20b8bfdaa
yfukuchi <yfukuchi@vitalify.jp>
2023-02-02 19:10:38 +0900

    並べ替えボタンの準備

--- src/components/shopList/index.jsx
+++ src/components/shopList/index.jsx
@@ -5,27 +5,25 @@ const ShopList = (props) => {
   const shopList = props.shopList;
   const router = useRouter();
   return (
-    <>
-      <ul>
-        {shopList.map((shop) => {
-          return (
-            <li key={shop["id"]} className="item-box">
-              <button
-                className="item-box__inner"
-                key={shop["id"]}
-                onClick={() => {
-                  router.push({
-                    pathname: `shops/${shop["id"]}`,
-                  });
-                }}
-              >
-                <Shop shop={shop}></Shop>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <ul>
+      {shopList.map((shop) => {
+        return (
+          <li key={shop["id"]} className="item-box">
+            <button
+              className="item-box__inner"
+              key={shop["id"]}
+              onClick={() => {
+                router.push({
+                  pathname: `shops/${shop["id"]}`,
+                });
+              }}
+            >
+              <Shop shop={shop}></Shop>
+            </button>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
